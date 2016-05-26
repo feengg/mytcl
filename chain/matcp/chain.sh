83837b49ea29548f2149962e6d8ef84941bd07d7
Ma Haicheng <hchma@outlook.com>
2016-05-26 21:00:15 +0800

    remove rt-fail part

---
+++ chain/matcp/chain.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# RECORD THE SIMLUATION TIME
+date > HopsResultAvg.txt
+date > HopsResultInst.txt
+date > q_length.txt
+
+# RUN FOUR SIMULATION WITH DIFFERENT CONGETIONTHRESHOLD UNDER THE SAME PATHLENGTH
+i=2
+echo SIMULATION DURATION: 300.0S, AODV+TCP >> HopsResultAvg.txt
+echo SIMULATION DURATION: 300.0S, AODV+TCP >> HopsResultInst.txt
+
+while [ $i -lt 21 ]; do
+    echo >> q_length.txt
+	echo --- Hop Node Number:$i --- >> q_length.txt
+	../../../matcp/matcp chain.tcl 1 $i 10 7 1 1 1 >> q_length.txt
+    let j=$i-1
+	echo FINISH $j simulations, start to analyze...
+	echo >> HopsResultAvg.txt
+	echo >> HopsResultInst.txt
+	echo --- Hop Node Number:$i ---	>> HopsResultAvg.txt
+	echo --- Hop Node Number:$i ---	>> HopsResultInst.txt
+	endnode=`expr $i - 1`
+	gawk -f ../../trace2stats_v05b/avgStatsForTcp.awk src=0 dst=$endnode flow=0 pkt=512 chain.tr >> HopsResultAvg.txt
+	gawk -f ../../trace2stats_v05b/instantThroughputForTcp.awk tic=1 src=0 dst=$endnode flow=0 pkt=512 chain.tr >> HopsResultInst.txt
+    let i=i+1     
+done
+
+rm *.tr
+rm *.nam
+rm tmp
+
+gawk -f ../../trace2stats_v05b/brief.awk flag="throughput" HopsResultAvg.txt > throughput
+gawk -f ../../trace2stats_v05b/brief.awk flag="delay" HopsResultAvg.txt > delay
---
+++ chain/runAll.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+str=`pwd`
+echo "current directory is: " $str
+
+cd matcp
+./chain.sh 
+#cd ../newreno
+#./chain.sh
+
+#cd ../semitcp
+#./chain.sh
+#cd ../tcpap
+#./chain.sh 
+
+cd ../
+./throughput.sh &
+./delay.sh
+
+echo "all jobs are finished, type any key to exit..."
+read c
